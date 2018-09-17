a0e8cca4e82d252409a572c51408c65c3f76258a
802709 <802709@NV034106.sylvaniaschools.local>
2018-09-17 13:22:06 -0400

    v4

---
+++ src/starfield/Character.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package starfield;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author 802709
+ */
+public class Character {
+    //no-args Consturctor
+    private int x;
+    private int y;
+    private int dx;
+    private int dy;
+    private Color color;
+    private int size;
+    private String name;
+
+    //constructor
+    public Character(){
+        x = 0;
+        y = 0;
+        dx=0;
+        dy=0;
+        color = Color.CYAN;
+        size = 20;
+        name = "enemy";
+    
+    }
+    public Character(int x, int y, Color color, int size, String name){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.size = size;
+        this.size = 20;
+        this.name = name;
+        dx=0;
+        dy=0;
+        this.name = "dude";
+
+    }
+    public Character(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    //Getters
+    public int getX() {
+        return x;
+            
+        }
+    public int getY (){
+        return y;
+    }
+        
+    public Color getColor () {
+        return color;
+    }
+    
+    public int getSize (){
+        return size;
+    }
+    public String getName (){
+        return name; 
+    }
+        
+    //Setters
+    public void setX(int x){
+        this.x = x;
+    }   
+    public void setY(int y){
+        this.y = y;
+    }   
+    
+      public void setDX(int dx) {
+        this.dx = dx;
+    }
+      
+       public void setDY(int dy) {
+        this.dy = dy;
+    }
+      
+    public void setColor(Color color){
+        this.color = color;
+    }   
+    
+    public void setSize(int size){
+        this.size = size;
+    } 
+    
+    public void setName (String name){
+        this.name = name;
+    }   
+    
+    //public methods 
+    //(how to perform actions/ "what does [hero, enemy, coin, etc.] do?"
+    public void draw(Graphics g){
+        g.setColor (color);
+        g.fillRect(x, y, size, size);
+    }
+    
+    public void kill() {
+        grow();
+    }
+    
+    public void move (int dx, int dy) {
+        x += dx * 3;
+        y += dy * 3;
+    }
+      public void update() {
+        move(dx, dy);
+        grow ();
+    }
+       /**
+     * Makes the character "bounce" and reverse direction on X axis 
+     */
+    public void reverseX() {
+        dx += -2;
+        //TODO Implement this method
+    }
+    
+    /**
+     * Makes the character "bounce" and reverse direction on Y axis 
+     */
+    public void reverseY() {
+        dy += -2;
+        //TODO Implement this method
+    } 
+    //private methods  
+    private void grow ()  {
+       //size =  (int) (Math.random()*10)- 4;
+       size *= 1.05;
+    }
+
+}
---
+++ src/starfield/Space.java
@@ -0,0 +1,145 @@
+
+package starfield;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+import java.awt.event.KeyEvent;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+/**
+ *
+ * @author 802709
+ */
+public class Space extends JPanel {
+    
+    final int marginX;
+    final int marginY;
+    private Hero hero;
+    private Enemy enemy;
+    private Timer timer;    
+    
+    public Space () {
+        super ();
+        marginX = 10;
+        marginY = 10;
+        hero = new Hero(600, 480, Color.CYAN, 20, "dude");
+        enemy = new Enemy (50, 50, Color.RED, 20, "Enemy");
+        timer = new Timer () ;      
+        timer.scheduleAtFixedRate(new ScheduleTask(), 100, 100);
+}
+    
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        this.setBackground(Color.BLACK);
+        
+         g.setColor(Color.WHITE);
+         g.setColor(Color.RED);
+         g.setColor(Color.GREEN);
+         g.setColor(Color.BLUE);
+         g.setColor(Color.CYAN);
+         g.setColor(Color.ORANGE);
+         g.setColor(Color.MAGENTA);
+         
+         drawStars(g);
+         
+         hero.draw(g);
+         enemy.draw(g);
+         //hero.kill();
+        //g.dispose();
+    }         
+      private class ScheduleTask extends TimerTask {	    	   
+        @Override
+        public void run() {
+            
+            hero.update();
+            enemy.update();
+            repaint();
+        }
+      }
+      public void keyPressed(KeyEvent e) {
+          if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+              hero.setDX(2);
+          }
+          else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+              hero.setDX(-2);
+          }
+           else if (e.getKeyCode() == KeyEvent.VK_UP) {
+              hero.setDY(-2);
+          }
+           else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+              hero.setDY(2);
+          }
+           
+           else if (e.getKeyCode() == KeyEvent.VK_D) {
+              hero.setDX(2);
+          }
+           else if (e.getKeyCode() == KeyEvent.VK_A) {
+              hero.setDX(-2);
+          }
+            else if (e.getKeyCode() == KeyEvent.VK_W) {
+              hero.setDX(-2);
+          }
+            else if (e.getKeyCode() == KeyEvent.VK_S) {
+              hero.setDY(2);
+          } 
+      }   
+        public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+            hero.setDX(0);
+        if (e.getKeyCode() == KeyEvent.VK_LEFT)
+            hero.setDX(0);
+        if (e.getKeyCode() == KeyEvent.VK_UP)
+            hero.setDY(0);
+        if (e.getKeyCode() == KeyEvent.VK_DOWN)
+            hero.setDY(0);
+        if (e.getKeyCode() == KeyEvent.VK_W)
+            hero.setDY(0);
+        if (e.getKeyCode() == KeyEvent.VK_A)
+            hero.setDY(0);
+        if (e.getKeyCode() == KeyEvent.VK_S)
+            hero.setDY(0);
+        if (e.getKeyCode() == KeyEvent.VK_D)
+            hero.setDY(0);
+        }
+   public void drawStars(Graphics g) {
+       int x = 0;
+       int y = 0;
+       for (int i  =  0;  i < 100; i++) {
+       
+       int rand1 = (int) (Math.random() * 960)  ;
+       int rand2 = (int) (Math.random() * 1200)  ;
+       //int rand3 = (int) (Math.random() * 100.0) + 1 ;
+        x = rand1 ;
+        y = rand2;
+        
+        g.fillOval (x , y , 3, 3);
+        
+        Color curr = g.getColor();
+          if (x % 2 == 0) 
+              g.setColor(curr.brighter());
+          else
+              g.setColor(curr.darker());
+          g.fillOval(x, y, 3, 3);
+        
+        
+        System.out.println(rand1+" "+rand2);   
+        }
+        //if (x > 1100 || y> 860) {
+           //break;
+        //}
+        }
+
+      /**
+     * Makes the hero and enemy bounce off walls
+     */    
+    private void wallCollissions() {
+        //TODO Implement this method
+    }
+   } 
+    
+
